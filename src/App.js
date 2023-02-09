cc4d6029031f830495d538a12219c270e9407ca4
youssef917 <o@gmail.com>
2023-02-09 17:41:17 +0200

    add About and Feature section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from "react";
-
+// import animate on scroll
+import Aos from "aos";
+import "aos/dist/aos.css";
 // import components
 import Hero from "./components/Hero";
+import About from "./components/About";
+import Features from "./components/Features";
 function App() {
+  Aos.init({
+    duration: 800,
+    offset: 0,
+  });
   return (
     <div className="overflow-hidden">
       <Hero />
+      <About />
+      <Features />
     </div>
   );
 }
